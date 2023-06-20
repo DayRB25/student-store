77d3f2511da68cde2bc9558eae51dee353831a3c
Ryan Brown <rkbrown25@meta.com>
2023-06-19 17:37:01 -0700

    Finished required project structure and state and function skeletons in App.jsx

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,11 +1,29 @@
-import * as React from "react"
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from "../Navbar/Navbar"
-import Sidebar from "../Sidebar/Sidebar"
-import Home from "../Home/Home"
-import "./App.css"
+import * as React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Sidebar from "../Sidebar/Sidebar";
+import Home from "../Home/Home";
+import "./App.css";
+import { useState } from "react";
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [shoppingCart, setShoppingCart] = useState([]);
+  const [checkoutForm, setCheckoutForm] = useState([]);
+
+  const handleOnToggle = () => {};
+
+  const handleAddItemToCart = (productId) => {};
+
+  const handleRemoveItemFromCart = (productId) => {};
+
+  const handleOnCheckoutFormChange = (name, value) => {};
+
+  const handleOnSubmitCheckoutForm = () => {};
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -13,9 +31,13 @@ export default function App() {
           {/* YOUR CODE HERE! */}
           <Navbar />
           <Sidebar />
-          <Home />
+          <Home
+            products={products}
+            handleAddItemToCart={handleAddItemToCart}
+            handleRemoveItemFromCart={handleRemoveItemFromCart}
+          />
         </main>
       </BrowserRouter>
     </div>
-  )
+  );
 }
