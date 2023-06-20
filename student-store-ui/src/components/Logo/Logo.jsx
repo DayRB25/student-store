f77d007dd25cc3cd2b50cc4c7bf35e726fc138d7
Ryan Brown <rkbrown25@meta.com>
2023-06-19 18:38:57 -0700

    Finished Logo component and Navbar css

---
+++ student-store-ui/src/components/Logo/Logo.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import "./Logo.css";
+import { Link } from "react-router-dom";
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <Link to={"/"}>
+        <img
+          src="https://codepath-student-store-demo.surge.sh/assets/codepath.f1b3e41a.svg"
+          alt="codepath logo"
+        />
+      </Link>
+    </div>
+  );
+}
