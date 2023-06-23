5f4c8bd792bee351617d0be2c6d811c572b4da3e
Ryan Brown <rkbrown25@meta.com>
2023-06-23 15:02:30 -0700

    Added invalid url page for all url's that do not match paths in react router setup

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -153,6 +153,10 @@ export default function App() {
                 />
               }
             />
+            <Route
+              path="*"
+              element={<p id="invalidURL">Ooops... Invalid url!</p>}
+            />
           </Routes>
         </main>
       </BrowserRouter>
