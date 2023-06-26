fb17b6aec8afcc324840b790c82b25db65e915c5
Ryan Brown <rkbrown25@meta.com>
2023-06-26 16:22:49 -0700

    File structure setup, modified run command in package json for clarity, and adding basic code to routes and model files

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,14 @@
-// YOUR CODE HERE
+const express = require("express");
+const cors = require("cors");
+const orderRoute = require("./routes/orders");
+const storeRoute = require("./routes/store");
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/orders", orderRoute);
+app.use("/api/store", storeRoute);
+
+module.exports = app;
---
+++ student-store-express-api/routes/orders.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const cors = require("cors");
+const StoreModel = require("../models/StoreModel");
+const router = require("express").Router();
+
+module.exports = router;
